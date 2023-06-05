832ca080b66cb781b494e182c7fb0cc5e83acfe5
lidan <lidandanino@h-MacBook-Pro-sl-lidan-2.local>
2023-06-05 18:34:19 +0300

    fix the connection

--- models/database.js
+++ models/database.js
@@ -3,56 +3,47 @@
  * Niv Netanel - 208540302
  */
 
-// validDay, validMonth and validYear are objects that define the validation criteria for day,
-const validDay = { type: Number, min: 1, max: 31 };
-const validMonth = { type: Number, min: 1, max: 12 };
-const validYear = { type: Number, min: 1900, max: 2100 };
-
-// enumCategory is an array of the possible categories for a cost.
-const enumCategory = ["food", "housing", "health", "sport", "education", "transportation", "other"];
-
-let crypto = require("crypto");
+const crypto = require("crypto");
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
-const connection = "mongodb+srv://lidan05463:lidan12345@cluster0.e1xynie.mongodb.net/FinalProject?retryWrites=true&w=majority";
-mongoose.connect(connection, { useNewUrlParser: true });
-const db = mongoose.connection;
-// Logging any errors that occur when connecting to the database.
-db.on("error", (error) => {
-  console.error("Error connecting to MongoDB Atlas:", error);
-});
-// Logging a success message once the connection is established.
-db.once("open", () => {
-  console.log("Successfully connected to MongoDB Atlas.");
+
+const connection =
+  "mongodb+srv://lidan05463:lidan12345@cluster0.e1xynie.mongodb.net/FinalProject?retryWrites=true&w=majority";
+
+const enumCategory = [
+  "food",
+  "housing",
+  "health",
+  "sport",
+  "education",
+  "transportation",
+  "other",
+];
+
+const valid = {
+  Day: { type: Number, min: 1, max: 31 },
+  Month: { type: Number, min: 1, max: 12 },
+  Year: { type: Number, min: 1900, max: 2100 },
+};
+
+const createSchemaFields = (additionalFields = {}) => ({
+  user_id: {
+    type: Number,
+    required: true,
+  },
+  day: valid.Day,
+  month: valid.Month,
+  year: valid.Year,
+  ...additionalFields,
 });
 
-// Creating schemas for the User, Costs and Reports collections.
-const usersSchema = new mongoose.Schema({ id: Number, first_name: String, last_name: String, birthday: Date }, { versionKey: false });
+const usersSchema = new mongoose.Schema(
+  { id: Number, first_name: String, last_name: String, birthday: Date },
+  { versionKey: false }
+);
 
 const computedReportsSchema = new mongoose.Schema(
-  {
-    user_id: {
-      type: Number,
-      required: true,
-    },
-    day: {
-      type: Number,
-      required: false,
-      min: validDay.min,
-      max: validDay.max,
-    },
-    month: {
-      type: Number,
-      required: false,
-      min: validMonth.min,
-      max: validMonth.max,
-    },
-    year: {
-      type: Number,
-      required: false,
-      min: validYear.min,
-      max: validYear.max,
-    },
+  createSchemaFields({
     category: {
       type: String,
       enum: enumCategory,
@@ -66,34 +57,12 @@ const computedReportsSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
-  },
+  }),
   { versionKey: false }
 );
 
 const costsSchema = new mongoose.Schema(
-  {
-    user_id: {
-      type: Number,
-      required: true,
-    },
-    day: {
-      type: Number,
-      required: false,
-      min: validDay.min,
-      max: validDay.max,
-    },
-    month: {
-      type: Number,
-      required: false,
-      min: validMonth.min,
-      max: validMonth.max,
-    },
-    year: {
-      type: Number,
-      required: false,
-      min: validYear.min,
-      max: validYear.max,
-    },
+  createSchemaFields({
     id: {
       type: String,
       unique: true,
@@ -114,67 +83,56 @@ const costsSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
-  },
+  }),
   { versionKey: false }
 );
 
-// Define Mongoose models for User, Cost, and Report
 const User = mongoose.model("User", usersSchema);
 const Cost = mongoose.model("Cost", costsSchema);
 const Report = mongoose.model("Report", computedReportsSchema);
 
-// Create a new User instance
-const user = new User({
-  id: 123123,
-  first_name: "moshe",
-  last_name: "israeli",
-  birthday: new Date(1990, 1, 10),
-});
+const createUserIfNotExist = async (user) => {
+  return User.findOne({ id: user.id }) || User.create(user);
+};
 
-// Function to create a new User if they do not already exist
-async function createUserIfNotExist(user) {
-  // Check if a User with the same ID already exists
-  let existUser = await User.findOne({ id: user.id });
-  // If the user does not exist, create a new User
-  if (!existUser) {
-    existUser = await User.create(user);
-  }
-  return existUser;
+const createNewEntry = async (Model, data) => {
+  const entry = new Model(data);
+  return await Model.create(entry);
+};
+
+async function createNewCost(costData) {
+  return await createNewEntry(Cost, costData);
 }
 
-// Call the createUserIfNotExist function and log the result to the console
-createUserIfNotExist(user).then(console.log);
-
-// Function to create a new Cost
-async function createNewCost(costUserId, costDay, costMonth, costYear, costDescription, costCategory, costSum) {
-  const cost = new Cost({
-    user_id: costUserId,
-    day: costDay,
-    month: costMonth,
-    year: costYear,
-    description: costDescription,
-    category: costCategory,
-    sum: costSum,
-  });
-
-  // Here is where we return the newly created cost
-  return await Cost.create(cost);
+async function createNewReport(reportData) {
+  return await createNewEntry(Report, reportData);
 }
 
-// Function to create a new Report
-async function createNewReport(reportUserId, reportDay, reportMonth, reportYear, reportDescription, reportCategory, reportSum) {
-  const report = new Report({
-    user_id: reportUserId,
-    day: reportDay,
-    month: reportMonth,
-    year: reportYear,
-    description: reportDescription,
-    category: reportCategory,
-    sum: reportSum,
-  });
-  await Report.create(report);
-}
-// Export the three models and two functions as a module
+mongoose
+  .connect(connection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to MongoDB Atlas.");
+
+    const user = new User({
+      id: 123123,
+      first_name: "moshe",
+      last_name: "israeli",
+      birthday: new Date(1990, 1, 10),
+    });
+
+    createUserIfNotExist(user).then(console.log);
+  })
+  .catch((error) => console.error("Error connecting to MongoDB Atlas:", error));
+
+const db = mongoose.connection;
+
+db.on("error", (error) =>
+  console.error("Error connecting to MongoDB Atlas:", error)
+);
+
 module.exports = {
   createNewCost,
   createNewReport,
