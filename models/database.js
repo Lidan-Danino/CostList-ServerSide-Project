865309fdb6038f282c7ee3d637ec14ba26bac8ce
lidan <lidandanino@h-MacBook-Pro-sl-lidan-2.local>
2023-06-18 11:57:54 +0300

    Last and final  update

--- models/database.js
+++ models/database.js
@@ -1,33 +1,40 @@
-/*Developers Details:
+/* Developers Details:
  * Lidan Danino - 207599473
  * Niv Netanel - 208540302
  */
 
-// validDay, validMonth and validYear are objects that define the validation criteria for day,
 const validDay = { type: Number, min: 1, max: 31 };
 const validMonth = { type: Number, min: 1, max: 12 };
 const validYear = { type: Number, min: 1900, max: 2100 };
-const connection = "mongodb+srv://lidan05463:lidan12345@cluster0.e1xynie.mongodb.net/FinalProject?retryWrites=true&w=majority";
+const connection =
+  "mongodb+srv://lidan05463:lidan12345@cluster0.e1xynie.mongodb.net/FinalProject?retryWrites=true&w=majority";
 
-// enumCategory is an array of the possible categories for a cost.
-const enumCategory = ["food", "housing", "health", "sport", "education", "transportation", "other"];
+const enumCategory = [
+  "food",
+  "housing",
+  "health",
+  "sport",
+  "education",
+  "transportation",
+  "other",
+];
 
 let crypto = require("crypto");
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 mongoose.connect(connection, { useNewUrlParser: true });
 const db = mongoose.connection;
-// Logging any errors that occur when connecting to the database.
 db.on("error", (error) => {
   console.error("Error connecting to MongoDB Atlas:", error);
 });
-// Logging a success message once the connection is established.
 db.once("open", () => {
   console.log("Successfully connected to MongoDB Atlas.");
 });
 
-// Creating schemas for the User, Costs and Reports collections.
-const usersSchema = new mongoose.Schema({ id: Number, first_name: String, last_name: String, birthday: Date }, { versionKey: false });
+const usersSchema = new mongoose.Schema(
+  { id: Number, first_name: String, last_name: String, birthday: Date },
+  { versionKey: false }
+);
 
 const computedReportsSchema = new mongoose.Schema(
   {
@@ -95,11 +102,9 @@ const costsSchema = new mongoose.Schema(
       max: validYear.max,
     },
     id: {
-      type: String,
+      type: Number,
       unique: true,
-      default: () => {
-        return crypto.randomUUID();
-      },
+      default: 1,
     },
     description: {
       type: String,
@@ -118,12 +123,10 @@ const costsSchema = new mongoose.Schema(
   { versionKey: false }
 );
 
-// Define Mongoose models for User, Cost, and Report
 const User = mongoose.model("User", usersSchema);
 const Cost = mongoose.model("Cost", costsSchema);
 const Report = mongoose.model("Report", computedReportsSchema);
 
-// Create a new User instance
 const user = new User({
   id: 123123,
   first_name: "moshe",
@@ -131,38 +134,55 @@ const user = new User({
   birthday: new Date(1990, 1, 10),
 });
 
-// Function to create a new User if they do not already exist
 async function createUserIfNotExist(user) {
-  // Check if a User with the same ID already exists
   let existUser = await User.findOne({ id: user.id });
-  // If the user does not exist, create a new User
   if (!existUser) {
     existUser = await User.create(user);
   }
   return existUser;
 }
 
-// Call the createUserIfNotExist function and log the result to the console
 createUserIfNotExist(user).then(console.log);
 
-// Function to create a new Cost
-async function createNewCost(costUserId, costDay, costMonth, costYear, costDescription, costCategory, costSum) {
+async function createNewCost(
+  costUserId,
+  costDay,
+  costMonth,
+  costYear,
+  costDescription,
+  costCategory,
+  costSum
+) {
+  const highestIdCost = await Cost.findOne().sort({ id: -1 }).limit(1);
+
+  let newId = 1;
+  if (highestIdCost) {
+    newId = highestIdCost.id + 1;
+  }
+
   const cost = new Cost({
     user_id: costUserId,
     day: costDay,
     month: costMonth,
     year: costYear,
+    id: newId,
     description: costDescription,
     category: costCategory,
     sum: costSum,
   });
 
-  // Here is where we return the newly created cost
-  return await Cost.create(cost);
+  return await cost.save();
 }
 
-// Function to create a new Report
-async function createNewReport(reportUserId, reportDay, reportMonth, reportYear, reportDescription, reportCategory, reportSum) {
+async function createNewReport(
+  reportUserId,
+  reportDay,
+  reportMonth,
+  reportYear,
+  reportDescription,
+  reportCategory,
+  reportSum
+) {
   const report = new Report({
     user_id: reportUserId,
     day: reportDay,
@@ -174,7 +194,7 @@ async function createNewReport(reportUserId, reportDay, reportMonth, reportYear,
   });
   await Report.create(report);
 }
-// Export the three models and two functions as a module
+
 module.exports = {
   createNewCost,
   createNewReport,
